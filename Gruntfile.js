29483f69c54c5b561e08bb5dcf1c84be2b53b31b
Kajal <kajal6633@gmail>
2016-06-03 09:53:27 -0700

    Basic files for starting angular

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		// Tasks
+		serve: {
+			options: {
+				port: 9100
+			}
+		},
+		sass: {                              // Task
+		    dist: {                            // Target
+		      options: {                       // Target options
+		        style: 'expanded',
+		        sourcemap: 'none'
+		      },
+		      files: {                         // Dictionary of files
+		        'WebContent/bom/app/css/bomPartdashboard.css': 'WebContent/bom/app/bom_part_dashboard_scss/main.scss' // 'destination': 'source'
+		      }
+		    }
+		  }
+		});
+	
+	// Load the plugin that provides the task.
+	grunt.loadNpmTasks('grunt-serve');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+
+	// Default task(s).
+	grunt.registerTask('appServe', ['serve']);//serve
+	//http://localhost:9000/app/index.html#/about
+	//http://localhost:9000/test/WebContent/index.html#/dms
+	grunt.registerTask('sass2css', ['sass']);
+	
+};
